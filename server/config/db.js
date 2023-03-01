e81e3ce1406ae2aee0cd82396eb1c51bd5e311dc
Jack Lam <jack15cm@gmail.com>
2023-03-02 04:32:36 +0800

    update db URI to work with mongo Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,6 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost:/books229"
+ // "URI": "mongodb://localhost:/books229"
+  "URI":"mongodb+srv://jack15cm:super15cm@cluster0.milbnzo.mongodb.net/books229?retryWrites=true&w=majority"
+
 };
